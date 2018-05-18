39bc1ea72df2b7976324e0da9edf5eedac811045
vit112358 <vitorff1234@gmail.com>
2018-05-18 19:08:42 +0000

    Update: Apagando comentários

--- src/main/java/br/ifmg/edu/trabalho_java_avancado/visao/VisaoPrincipal_Vendedores.java
+++ src/main/java/br/ifmg/edu/trabalho_java_avancado/visao/VisaoPrincipal_Vendedores.java
@@ -105,37 +105,6 @@ public class VisaoPrincipal_Vendedores extends javax.swing.JFrame {
         setLocationRelativeTo(null);
     }// </editor-fold>//GEN-END:initComponents
 
-    /**
-     * @param args the command line arguments
-     */
-    
-    /*
-    public static void main(String args[]) {
-        
-        try {
-            for (javax.swing.UIManager.LookAndFeelInfo info : javax.swing.UIManager.getInstalledLookAndFeels()) {
-                if ("Windows".equals(info.getName())) {
-                    javax.swing.UIManager.setLookAndFeel(info.getClassName());
-                    break;
-                }
-            }
-        } catch (ClassNotFoundException ex) {
-            java.util.logging.Logger.getLogger(VisaoPrincipal_Vendedores.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
-        } catch (InstantiationException ex) {
-            java.util.logging.Logger.getLogger(VisaoPrincipal_Vendedores.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
-        } catch (IllegalAccessException ex) {
-            java.util.logging.Logger.getLogger(VisaoPrincipal_Vendedores.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
-        } catch (javax.swing.UnsupportedLookAndFeelException ex) {
-            java.util.logging.Logger.getLogger(VisaoPrincipal_Vendedores.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
-        }
-
-        java.awt.EventQueue.invokeLater(new Runnable() {
-            public void run() {
-                new VisaoPrincipal_Vendedores().setVisible(true);
-            }
-        });
-    }*/
-
     // Variables declaration - do not modify//GEN-BEGIN:variables
     private javax.swing.JLabel jLabel1;
     private javax.swing.JLabel jLabel3;
