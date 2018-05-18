fa9336a6cc950ea78c4df44972b418bf8a758223
vit112358 <vitorff1234@gmail.com>
2018-05-18 19:07:18 +0000

    Update: Apagando comentários

--- src/main/java/br/ifmg/edu/trabalho_java_avancado/visao/VisaoPrincipal_Vendedores.java
+++ src/main/java/br/ifmg/edu/trabalho_java_avancado/visao/VisaoPrincipal_Vendedores.java
@@ -1,8 +1,3 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package br.ifmg.edu.trabalho_java_avancado.visao;
 
 import java.awt.event.ActionEvent;
@@ -18,7 +13,7 @@ import javax.swing.Timer;
 public class VisaoPrincipal_Vendedores extends javax.swing.JFrame {
 
     /**
-     * Creates new form VisaoPrincipal
+     * Creates new form VisaoPrincipal_Vendedores
      */
     public VisaoPrincipal_Vendedores() {
         initComponents();
